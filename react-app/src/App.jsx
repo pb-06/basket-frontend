66d8a496ee9798d96af5769d94917c99045d75b5
pb-06 <kkandomail@gmail.com>
2025-09-17 14:29:38 +0200

    Prepare handleButtonClick

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,22 +1,51 @@
 import React from "react";
 
 export default class App extends React.Component {
+  state = {
+    cart: {
+      items: [
+        { name: 'Laptop', price: 1200, quantity: 1 },
+        { name: 'Headphone', price: 200, quantity: 1 }
+      ]
+    },
+    nev: null,
+    ar: null,
+    db: null,
+  }
+
+  handleButtonClick = e => {
+    e.preventDefault();
+
+    // TODO - implement cart item insertion
+  }
+
   render() {
     return (
       <div>
         <form>
           <h1>Termék hozzáadása a kosárhoz</h1>
           <label htmlFor="input_nev">Termék neve: </label>
-          <input type="text" name="input_nev" id="input_nev" placeholder="Név..." /><br />
+          <input type="text" name="input_nev" id="input_nev" placeholder="Név..." onChange={value=>this.setState({nev: value})} /><br />
           <label htmlFor="input_ar">Ár: </label>
-          <input type="number" name="input_ar" id="input_ar" value={0} /><br />
+          <input type="number" name="input_ar" id="input_ar" value={0} onChange={value=>this.setState({ar: value})} /><br />
           <label htmlFor="input_db">Mennyiség: </label>
-          <input type="number" name="input_db" id="input_db" value={0} /><br />
-          <input type="button" id="btn_submit" name="btn_submit" value="Hozzáadás a kosárhoz" />
+          <input type="number" name="input_db" id="input_db" value={0} onChange={value=>this.setState({db: value})} /><br />
+          <input type="button" id="btn_submit" name="btn_submit" value="Hozzáadás a kosárhoz" onClick={this.handleButtonClick} />
         </form>
         <hr />
         <h3>A kosár tartalma</h3>
-        <ul></ul>
+
+        {
+          this.state.cart.items.length >= 1 &&
+          <ul>
+            {
+              this.state.cart.items.map((item, idx)=><li key={idx}>
+                {item.name} - Ár: {item.price} Ft, Mennyiség: {item.quantity}
+              </li>)
+            }
+          </ul>
+        }
+
       </div>
     )
   }
